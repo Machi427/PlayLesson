9c8080d3ef10419f86d2fde9db25dac7788f2844
Machirda427 <machirda427@gmail.com>
2015-07-19 00:06:27 +0900

    2.3.2

--- app/models/Message.java
+++ app/models/Message.java
@@ -1,13 +1,15 @@
 package models;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import javax.persistence.*;
 import javax.validation.*;
 
+import com.avaje.ebean.annotation.*;
+
 import play.db.ebean.*;
 import play.data.validation.*;
+import play.data.validation.Constraints.*;
 
 @Entity
 public class Message extends Model {
@@ -18,4 +20,14 @@ public class Message extends Model {
 	public String mail;
 	public String message;
 	public Date postdate;
+	
+	public static Finder<Long, Message> find = 
+		new Finder<Long, Message>(Long.class, Message.class);
+	
+	@Override
+	public String toString(){
+		return ("[id:" + id + ", name:" + name + ", mail:" + mail +
+			", message:" + message + ", date:" + postdate + "]");
+	}
+
 }
